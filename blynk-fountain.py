02f5df2d4f90d7e5bd48d87e365973aa6b90b71c
MrVu <chuvi0902@gmail.com>
2019-05-25 20:34:04 +0700

    gpio

--- blynk-fountain.py
+++ blynk-fountain.py
@@ -3,13 +3,15 @@ import RPi.GPIO as GPIO
 blynk = BlynkLib.Blynk('a49e0d5f2af8495a9bd5ed7044b31fc5')
 GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD
 GPIO.setup(16, GPIO.OUT)           # set GPIO24 as an output
-
+GPIO.setup(20, GPIO.OUT)
 @blynk.VIRTUAL_WRITE(1)
 def my_write_handler(value):
     print('Current V1 value: {}'.format(value))
 
 @blynk.VIRTUAL_WRITE(2)
 def led_switch(value):
+    GPIO.output(16, value)
+    GPIO.output(20, value)
     print('Current V2 value: {}'.format(value))
 
 while True:
