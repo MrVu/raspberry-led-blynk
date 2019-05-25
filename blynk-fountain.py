6885e0b3321dec4a6faa63fcb00405220eecfa9f
MrVu <chuvi0902@gmail.com>
2019-05-26 00:54:40 +0700

    update

--- blynk-fountain.py
+++ blynk-fountain.py
@@ -1,13 +1,16 @@
 import BlynkLib
+import BlynkTimer
 import RPi.GPIO as GPIO
 from time import sleep
 blynk = BlynkLib.Blynk('a49e0d5f2af8495a9bd5ed7044b31fc5')
 GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD
 GPIO.setup(16, GPIO.OUT)           # set GPIO24 as an output
 GPIO.setup(20, GPIO.OUT)
+
 @blynk.VIRTUAL_WRITE(1)
 def my_write_handler(value):
-    print('Current V1 value: {}'.format(value))
+    global valueV1
+    valueV1 = value
 
 @blynk.VIRTUAL_WRITE(2)
 def led_switch(value):
@@ -15,11 +18,7 @@ def led_switch(value):
     GPIO.output(16, int_value)
     GPIO.output(20, int_value)
     print('Current V2 value: {}'.format(value))
-    sleep(5)
-
-@blynk.sync_virtual(1,2)
-def get_all_val(value):
-    print(value)
+    print('Current V1 value: {}'.format(valueV1))
 
 
 while True:
